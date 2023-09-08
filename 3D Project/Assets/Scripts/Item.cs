3d62ce9e10740a7dbf92a7e35c2e3fa616a4a8dc
nadong <ehdgns4025@gmail.com>
2023-09-08 21:25:50 +0900

    골드메탈 아이템 생성까지

---
+++ 3D Project/Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon };
+    public Type type;
+    public int value;
+
+}
